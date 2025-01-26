b8bc5ee3bc4b0f995025b376bb15824165ef6983
Hyuniverse <hyuniverse.dev@gmail.com>
2025-01-26 11:52:13 +0900

    enum2

--- src/ch02/sec02/EnumExample2.java
+++ src/ch02/sec02/EnumExample2.java
@@ -5,14 +5,32 @@ public class EnumExample2 {
         // 현재 누적 구매 금액을 기준으로 회원 등급 분기하기
         int member1 = 100000;
         int member2 = 50000;
-        
+
+        // 배열 활용
+        int[] members = {member1, member2};
+
         // 등급 구간
         //  - 10000원 미만: GENERAL
         //  - 10000원 이상: SILVER
         //  - 30000원 이상: GOLD
         //  - 40000원 이상: VIP
         //  - 70000원 이상: VVIP
-        
-        // 각 member 등급을 Enum 타입을 사용해서 출력
+        MemberGrade grade;
+        int count = 1;
+        for (int member : members) {
+            if (member >= 70000) {
+                grade = MemberGrade.VVIP;
+            } else if (member >= 40000) {
+                grade = MemberGrade.VIP;
+            } else if (member >= 30000) {
+                grade = MemberGrade.GOLD;
+            } else if (member >= 10000) {
+                grade = MemberGrade.SILVER;
+            } else {
+                grade = MemberGrade.GENERAL;
+            }
+            System.out.println("member" + count + " 회원의 등급은 = " + grade);
+            count++;
+        }
     }
 }
--- src/ch02/sec02/MemberGrade.java
+++ src/ch02/sec02/MemberGrade.java
@@ -2,4 +2,9 @@ package ch02.sec02;
 
 public enum MemberGrade {
     // 회원 등급 - VVIP, VIP, GOLD, SILVER, GENERAL
+    VVIP,
+    VIP,
+    GOLD,
+    SILVER,
+    GENERAL
 }
