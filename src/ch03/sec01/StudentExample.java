6015f4d8771b826f747a0d353740db9599523359
Hyuniverse <hyuniverse.dev@gmail.com>
2025-02-01 09:07:27 +0900

    20250201

--- src/ch03/sec01/StudentExample.java
+++ src/ch03/sec01/StudentExample.java
@@ -4,7 +4,15 @@ public class StudentExample {
     public static void main(String[] args) {
         // Student 클래스(설계도)를 통해서 인스턴스 만들기(물리적인 객체)
         Student student = new Student();
+        System.out.println("student1 = " + student);
+
+        Student student2 = new Student();
+        System.out.println("student2 = " + student2);
 
         Teacher teacher = new Teacher();
+        System.out.println("teacher1 = " + teacher);
+
+        Teacher teacher2 = new Teacher();
+        System.out.println("teacher2 = " + teacher2);
     }
 }
