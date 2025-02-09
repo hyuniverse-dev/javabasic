a4c8c166d98361bc2e30dc4e62dcf9e6aa60d11e
Hyuniverse <hyuniverse.dev@gmail.com>
2025-02-09 12:33:23 +0900

    추상클래스 연습

---
+++ src/ch04/sec04/package2/Animal.java
@@ -0,0 +1,6 @@
+package ch04.sec04.package2;
+
+// 추상클래스
+public class Animal {
+    // 추상 메소드 sound() 선언
+}
---
+++ src/ch04/sec04/package2/Dog.java
@@ -0,0 +1,5 @@
+package ch04.sec04.package2;
+
+public class Dog extends Animal {
+    // 추상클래스 Animal의 sound() 추상메소드 재정의
+}
